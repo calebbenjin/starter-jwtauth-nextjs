1945524fbc454fc8df88a19d82cd89edb0f53457
calebbenjin <calebben94@gmail.com>
2022-03-19 15:26:00 +0100

    added parseCookie

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,7 +1,37 @@
-export default function Page({ }) {
+import { parseCookies } from '@/helpers/index';
+
+
+export default function Page({ token }) {
+
+  //  We now have access to the token
+  console.log(token)
+
   return (
     <main className="dashboard">
       <h1>User Dashboard</h1>  
     </main>
   )
+}
+
+
+export async function getServerSideProps({ req }) {
+
+  const { token } = parseCookies(req);
+
+  // Redirecting the user if the token is not found
+  if (!token) {
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false,
+      },
+    };
+  }
+
+
+  return {
+    props: {
+      token
+    },
+  };
 }
